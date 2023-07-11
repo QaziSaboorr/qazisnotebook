3399dc008b70849d6b685076e04e91e0a4620b81
QaziSaboorr <qazi.ali@ucalgary.ca>
2023-07-11 03:58:22 -0600

    make it better

--- backend/index.js
+++ backend/index.js
@@ -6,18 +6,24 @@ connectToMongo(); //running the imported function from /db
 const app = express(); //app is using express server
 const port = 8000; //server running on port 8000
 var cors = require("cors");
-
+const path = require("path");
 app.use(cors());
 
 app.use(express.json()); //middleware to use json as data exchange between client(frontend) and server(backend)
 
-app.get("/", (req, res) => {
-  res.send("Hello World!"); //res == what you send from back end to server
-});
+// app.get("/", (req, res) => {
+//   res.send("Hello World!"); //res == what you send from back end to server
+// });
 //Available routes
 app.use("/api/auth", require("./routes/auth")); //importing the route from the path specified in "require" function
 app.use("/api/notes", require("./routes/notes")); //imorting the route again from s
 
+__dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, "/frontend/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+});
 app.listen(port, () => {
   console.log(`Inotebook backend listening on http://localhost:${port}`);
 });
