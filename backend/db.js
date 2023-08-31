3eb7b751f2e3a0d3ec538bccc68df37881129079
QaziSaboorr <qazi.ali@ucalgary.ca>
2023-08-31 07:49:12 -0600

    adding environment variable

--- backend/db.js
+++ backend/db.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose"); //using mongoose module which we installed when we used npm innit "reffer to harry video of you forget"
-const mongoURI = "mongodb://localhost:27017/inotebook"; //we got this link from our mongodb compass "/inotebook" represents database
+const env = require("dotenv");
+
 mongoose.set("strictQuery", true); // this was just to avoid warning
 const connectToMongo = async () => {
-  mongoose.connect(mongoURI, () => {
+  mongoose.connect(process.env.MONGO_URI, () => {
     console.log("connected to moongo hogaya succesfully");
   });
 };
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,8 @@
 // mongodb://localhost:27017
 const express = require("express");
+const dotenv = require("dotenv");
 const connectToMongo = require("./db");
+dotenv.config();
 connectToMongo(); //running the imported function from /db
 
 const app = express(); //app is using express server
@@ -18,9 +20,6 @@ app.use(express.json()); //middleware to use json as data exchange between clien
 app.use("/api/auth", require("./routes/auth")); //importing the route from the path specified in "require" function
 app.use("/api/notes", require("./routes/notes")); //imorting the route again from s
 
-
 app.listen(port, () => {
   console.log(`Inotebook backend listening on http://localhost:${port}`);
 });
-
-
